7f5d72a12c517034cdba88e3ae17216a742bde69
Gabriel Lustosa Queiroz <lustosaki2@gmail.com>
2022-06-05 14:05:32 -0300

    Criando rating search

--- educa/apps/rating/urls.py
+++ educa/apps/rating/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('view/<int:course_id>/', views.RatingView.as_view(), name='view'),
     path('render/create/<int:course_id>/', views.RatingRenderCreateView.as_view(), name='render_create'),
     path('create/<int:course_id>/', views.RatingCreateView.as_view(), name='create'),
+    path('search<int:course_id>/', views.RatingSearchView.as_view(), name='search'),
 ]
--- educa/apps/rating/views.py
+++ educa/apps/rating/views.py
@@ -28,6 +28,7 @@ class RatingView(
 
         course = self.get_course()
         context['course'] = course
+        context['page_url'] = f'/course/rating/view/{course.id}'
 
         self.request.session[f'section-{course.id}'] = 'rating'
 
@@ -77,3 +78,35 @@ class RatingCreateView(
         Rating.objects.create(rating=rating, comment=comment, user=request.user, course=course)
 
         return redirect(reverse('rating:view', kwargs={'course_id': course.id}))
+
+
+class RatingSearchView(RatingView):
+
+    def get_queryset(self):
+        search = self.request.GET.get('search')
+        filter_rating = self.request.GET.get('filter')
+        queryset = Rating.objects.filter(course=self.get_course(), comment__icontains=search)
+
+        if filter_rating != 'all':
+            queryset = queryset.filter(rating__in=[int(filter_rating), (float(filter_rating) + .5)])
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        course = self.get_course()
+
+        context['page_url'] = f'/course/rating/search/{course.id}/'
+        context['search_term'] = self.request.GET.get('search')
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        search = self.request.GET.get('search')
+
+        course = self.get_course()
+
+        if search == "":
+            return redirect(reverse('rating:view', kwargs={'course_id': course.id}))
+        return super().get(request, *args, **kwargs)
